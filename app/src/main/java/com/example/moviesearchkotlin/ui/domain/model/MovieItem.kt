1ad835981349c12158eff3aab21fea5b5911cbc2
Daniel Nugent <skatesf@gmail.com>
2020-09-18 13:49:36 -0700

    update kotlin and gradle

--- app/src/main/java/com/example/moviesearchkotlin/ui/domain/model/MovieItem.kt
+++ app/src/main/java/com/example/moviesearchkotlin/ui/domain/model/MovieItem.kt
@@ -1,8 +1,12 @@
 package com.example.moviesearchkotlin.ui.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MovieItem(
     val movieId: String,
     val moviePosterUrl: String,
     val movieTitle: String,
     val movieYear: String
-)
+) : Parcelable
